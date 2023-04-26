88181f4cbacb8d90de46a94a05b9815c63d850ac
KEVIN TANN <u1259625@umail.utah.edu>
2023-04-26 15:12:40 -0600

    Base UI and Images, need to update correct imgs

--- aboutwindow.cpp
+++ aboutwindow.cpp
@@ -6,7 +6,16 @@ AboutWindow::AboutWindow(QWidget *parent) :
     ui(new Ui::AboutWindow)
 {
     ui->setupUi(this);
-    connect(ui->backButton, QPushButton::clicked, this, AboutWindow::backButtonClicked);
+    connect(ui->backButton, &QPushButton::clicked, this, &AboutWindow::backButtonClicked);
+
+    QPixmap mainGameImage(":/HelpWindowImages/HelpWindowImages/feedback.PNG");
+    QPixmap difficultyImage(":/HelpWindowImages/HelpWindowImages/GuessArea.PNG");
+    QPixmap studyImage(":/HelpWindowImages/HelpWindowImages/hintarea.PNG");
+    QPixmap statsImage(":/HelpWindowImages/HelpWindowImages/hintarea.PNG");
+    ui->mainGameImage->setPixmap(mainGameImage.scaled(ui->mainGameImage->size(), Qt::KeepAspectRatio,Qt::SmoothTransformation));
+    ui->difficultyImage->setPixmap(difficultyImage.scaled(ui->difficultyImage->size(), Qt::KeepAspectRatio,Qt::SmoothTransformation));
+    ui->studyImage->setPixmap(studyImage.scaled(ui->studyImage->size(), Qt::KeepAspectRatio,Qt::SmoothTransformation));
+    ui->statsImage->setPixmap(statsImage.scaled(ui->statsImage->size(), Qt::KeepAspectRatio,Qt::SmoothTransformation));
 }
 
 AboutWindow::~AboutWindow()
